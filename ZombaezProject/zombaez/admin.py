faeb2426cd632f656a5091dee9f4ffe494107159
KB9 <kavanbickerstaff@googlemail.com>
2016-03-08 20:14:13 +0000

    Tabulated admin models

--- ZombaezProject/zombaez/admin.py
+++ ZombaezProject/zombaez/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from zombaez.models import User, Game, Badge
 
-admin.site.register(User)
-admin.site.register(Game)
-admin.site.register(Badge)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'zombies_killed', 'total_games_played',
+            'total_ammo_collected', 'largest_party_size', 'total_food_collected',
+            'total_days_survived', 'avatar'
+    )
+
+class BadgeAdmin(admin.ModelAdmin):
+    list_display = ('badge_id', 'user', 'description', 'level', 'name',
+            'image', 'requirements'
+    )
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('user', 'time_of_day', 'party_size', 'ammo_count',
+            'food_count', 'days_survived'
+    )
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Game, GameAdmin)
+admin.site.register(Badge, BadgeAdmin)
