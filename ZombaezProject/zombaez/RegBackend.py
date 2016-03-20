5e13f5d6f8733274b1d6881f7631804aff89042a
David Boyd <davidboyd96@gmail.com>
2016-03-20 22:25:50 +0000

    Pickling works 100%

--- ZombaezProject/zombaez/RegBackend.py
+++ ZombaezProject/zombaez/RegBackend.py
@@ -1,13 +1,16 @@
-from zombaez.models import User
+from zombaez.models import User, Game
 from registration.signals import user_registered
 from registration.forms import RegistrationForm
 
 def user_created(sender, user, request, **kwargs):
 	form = RegistrationForm(request.POST)
-	#data = User(user=user, avatar = request.FILES['picture'])
-	data = User(user=user)
-	data.username = user.username
-	data.save()
+	#userData = User(user=user, avatar = request.FILES['picture'])
+	userData = User(user=user)
+	userData.username = user.username
+	userData.save()
+	gameData = Game(user=request.user.user)
+	gameData.save()
+	
 
 user_registered.connect(user_created)
 
