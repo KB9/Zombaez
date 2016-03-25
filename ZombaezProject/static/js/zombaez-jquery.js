ba31397ee664b7bba5b2acffc7f6291e9e778ce8
KB9 <kavanbickerstaff@googlemail.com>
2016-03-25 19:04:24 +0000

    Removed debug code from AJAX call

--- ZombaezProject/static/js/zombaez-jquery.js
+++ ZombaezProject/static/js/zombaez-jquery.js
@@ -1,19 +1,6 @@
 $(document).ready(function() {
-    
-    $("#play-button").click(function(event) {
-        $.ajax({
-            type: "GET",
-            url: "/zombaez/game_event/",
-            data: {"var": "dank game started"},
-            success: function(data) {
-                $("#play-button").html(data);
-            },
-            error: function(data) {
-                alert("Failed to connect to engine!")
-            }
-        });
-    });
 
+    // Used to access key events in main JS
     $(document).keypress(function(event) {
         onKeyPressed(event.charCode);
     });
